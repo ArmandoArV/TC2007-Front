9011fc2fb898952f406370f53a576b237bcd8dc0
ArmandoAV <armando.arredondo.valle@gmail.com>
2023-10-19 18:06:30 -0600

    YA FUNCA

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -5,48 +5,56 @@ import styles from "./page.module.css";
 const Swal = require("sweetalert2");
 
 export default function Home() {
-  const [nombre_proyecto, setNombre_proyecto] = useState("");
-  const [categoria_proyecto, setCategoria_proyecto] = useState("");
-  const [descripcion_proyecto, setDescripcion_proyecto] = useState("");
-  const [tipo_proyecto, setTipo_proyecto] = useState("");
-  const [estado, setEstado] = useState("");
-  const [latitud, setLatitud] = useState(0);
-  const [longitud, setLongitud] = useState(0);
-  const [url_proyecto, setUrl_proyecto] = useState("");
-  const [imagen, setImagen] = useState(null);
-  const [logo, setLogo] = useState(null);
-  const [horario_apertura, setHorario_apertura_proyecto] = useState("");
-  const [horario_cierre, setHorario_cierre_proyecto] = useState("");
+  const [projectData, setProjectData] = useState({
+    nombre_proyecto: "",
+    categoria_proyecto: "",
+    descripcion_proyecto: "",
+    tipo_proyecto: "",
+    estado: "",
+    latitud: 0,
+    longitud: 0,
+    url_proyecto: "",
+    imagen: null,
+    logo: null,
+    horario_apertura_proyecto: "",
+    horario_cierre_proyecto: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setProjectData({
+      ...projectData,
+      [name]: value,
+    });
+  };
+
+  const handleFileInputChange = (event) => {
+    const { name, files } = event.target;
+    setProjectData({
+      ...projectData,
+      [name]: files[0],
+    });
+  };
 
   const handleSave = useCallback(() => {
-    const requestBody = {
-      nombre_proyecto: nombre_proyecto,
-      categoria_proyecto: categoria_proyecto,
-      descripcion_proyecto: descripcion_proyecto,
-      tipo_proyecto: tipo_proyecto,
-      estado: estado,
-      latitud: parseFloat(latitud),
-      longitud: parseFloat(longitud),
-      url_proyecto: url_proyecto,
-      // imagen: imagen,
-      // logo: logo,
-      horario_apertura_proyecto: horario_apertura,
-      horario_cierre_proyecto: horario_cierre,
-    };
+    const formData = new FormData();
+    for (const key in projectData) {
+      formData.append(key, projectData[key]);
+    }
 
-    console.log("Request Body:", requestBody);
+    console.log("Request Body:", formData);
 
     fetch(`${API_URL}/proyecto`, {
       method: "POST",
       headers: {
-        "Content-Type": "multipart/form-data",
+        // Remove "Content-Type" header to let the browser set it automatically
         "x-access-token": localStorage.getItem("token") || "",
       },
-      body: requestBody,
+      body: formData,
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Response Body:", data); // Print the response body
+        console.log("Response Body:", data);
         if (data.mensaje === "proyecto agregado") {
           Swal.fire({
             icon: "success",
@@ -65,20 +73,7 @@ export default function Home() {
       .catch((error) => {
         console.error("Error:", error);
       });
-  }, [
-    nombre_proyecto,
-    categoria_proyecto,
-    descripcion_proyecto,
-    tipo_proyecto,
-    estado,
-    latitud,
-    longitud,
-    url_proyecto,
-    imagen,
-    logo,
-    horario_apertura,
-    horario_cierre,
-  ]);
+  }, [projectData]);
 
   return (
     <main className="flex min-h-screen flex-row items-center justify-between p-2">
@@ -95,7 +90,7 @@ export default function Home() {
             type="text"
             name="nombre_proyecto"
             id="nombre_proyecto"
-            onChange={(e) => setNombre_proyecto(e.target.value)}
+            onChange={handleInputChange}
           />
           <label className={styles.label} htmlFor="description">
             Descripcion
@@ -107,7 +102,7 @@ export default function Home() {
             id="description"
             cols={30}
             rows={10}
-            onChange={(e) => setDescripcion_proyecto(e.target.value)}
+            onChange={handleInputChange}
           />
           <label className={styles.label} htmlFor="address">
             Estado
@@ -119,8 +114,8 @@ export default function Home() {
             className="border border-gray-300 p-2"
             name="estado"
             id="address"
-            onChange={(e) => setEstado(e.target.value)}
-            value={estado}
+            onChange={handleInputChange}
+            value={projectData.estado}
           >
             <option value="0">Selecciona un estado</option>
             {ESTADOS_DE_MEXICO.map((estado) => (
@@ -139,7 +134,7 @@ export default function Home() {
             name="imagen"
             id="imagen"
             accept="image/*"
-            onChange={(e) => setImagen(e.target.files[0])}
+            onChange={handleFileInputChange}
           />
           <label className={styles.label} htmlFor="logo">
             Logo
@@ -150,7 +145,7 @@ export default function Home() {
             name="logo"
             id="logo"
             accept="image/*"
-            onChange={(e) => setLogo(e.target.files[0])}
+            onChange={handleFileInputChange}
           />
           <label className={styles.label} htmlFor="latitud">
             Latitud
@@ -163,7 +158,7 @@ export default function Home() {
             type="number"
             name="latitud"
             id="latitud"
-            onChange={(e) => setLatitud(e.target.value)}
+            onChange={handleInputChange}
           />
           <label className={styles.label} htmlFor="longitud">
             Longitud
@@ -176,7 +171,7 @@ export default function Home() {
             type="number"
             name="longitud"
             id="longitud"
-            onChange={(e) => setLongitud(e.target.value)}
+            onChange={handleInputChange}
           />
           <label className={styles.label} htmlFor="tipo_proyecto">
             Tipo
@@ -186,10 +181,10 @@ export default function Home() {
           </label>
           <select
             className="border border-gray-300 p-2"
-            name="tipo_proyecto" // Match the state property name
+            name="tipo_proyecto"
             id="tipo_proyecto"
-            onChange={(e) => setTipo_proyecto(e.target.value)}
-            value={tipo_proyecto}
+            onChange={handleInputChange}
+            value={projectData.tipo_proyecto}
           >
             <option value="">Selecciona un tipo</option>
             {TIPOS.map((tipo) => (
@@ -209,8 +204,8 @@ export default function Home() {
             className="border border-gray-300 p-2"
             name="categoria_proyecto"
             id="categoria_proyecto"
-            onChange={(e) => setCategoria_proyecto(e.target.value)}
-            value={categoria_proyecto}
+            onChange={handleInputChange}
+            value={projectData.categoria_proyecto}
           >
             <option value="0">Selecciona una categoria</option>
             {CATEGORIAS.map((categoria) => (
@@ -227,7 +222,7 @@ export default function Home() {
             type="text"
             name="url_proyecto"
             id="url_proyecto"
-            onChange={(e) => setUrl_proyecto(e.target.value)}
+            onChange={handleInputChange}
           />
           <label className={styles.label} htmlFor="horario_apertura_proyecto">
             Horario de apertura
@@ -237,7 +232,7 @@ export default function Home() {
             type="time"
             name="horario_apertura_proyecto"
             id="horario_apertura_proyecto"
-            onChange={(e) => setHorario_apertura_proyecto(e.target.value)}
+            onChange={handleInputChange}
           />
           <label className={styles.label} htmlFor="horario_cierre_proyecto">
             Horario de cierre
@@ -247,7 +242,7 @@ export default function Home() {
             type="time"
             name="horario_cierre_proyecto"
             id="horario_cierre_proyecto"
-            onChange={(e) => setHorario_cierre_proyecto(e.target.value)}
+            onChange={handleInputChange}
           />
 
           <button
